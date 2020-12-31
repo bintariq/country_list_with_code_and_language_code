77ef85d67a2b611d9d4b13f92284c742f9686798
rehan <web@invictussolutions.co>
2020-12-30 22:54:45 -0800

    Country IF

---
+++ if_condition_for_country_match.php
@@ -0,0 +1,292 @@
+if($country == "DJ" || $country == "ER" || $country == "ET"){
+        $lang = "aa";
+      }
+      if(
+        $country == "AE"||
+        $country == "BH"||
+        $country == "DZ"||
+        $country == "EG"||
+        $country == "IQ"||
+        $country == "JO"||
+        $country == "KW"||
+        $country == "LB"||
+        $country == "LY"||
+        $country == "MA"||
+        $country == "OM"||
+        $country == "QA"||
+        $country == "SA"||
+        $country == "SD"||
+        $country == "SY"||
+        $country == "TN"||
+        $country == "YE"
+        ){
+        $lang = "ar";
+      }
+      if($country == "AZ"){
+          $lang = "az";
+      }
+      if($country == "BY"){
+          $lang = "be";
+      }
+      if($country == "BG"){
+          $lang = "bg";
+      }
+      if($country == "BD"){
+          $lang = "bn";
+      }
+      if($country == "BA"){
+          $lang = "bs";
+      }
+      if($country == "CZ"){
+          $lang = "cs";
+      }
+      if($country == "DK"){
+          $lang = "da";
+      }
+      if(
+        $country == "AT"||
+        $country == "CH"||
+        $country == "DE"||
+        $country == "LU"
+        )
+      {
+          $lang = "de";
+      }
+      if($country == "MV"){
+          $lang = "dv";
+      }
+      if($country == "BT"){
+          $lang = "dz";
+      }
+      if($country == "GR"){
+          $lang = "el";
+      }
+      if(
+        $country == "AG" ||
+        $country == "AI" ||
+        $country == "AQ" ||
+        $country == "AS" ||
+        $country == "AU" ||
+        $country == "BB" ||
+        $country == "BW" ||
+        $country == "CA" ||
+        $country == "GB" ||
+        $country == "IE" ||
+        $country == "KE" ||
+        $country == "NG" ||
+        $country == "NZ" ||
+        $country == "PH" ||
+        $country == "SG" ||
+        $country == "US" ||
+        $country == "ZA" ||
+        $country == "ZM" ||
+        $country == "ZW"
+        )
+      {
+        $lang = "en";
+      }
+      if(
+        $country == "AD"||
+        $country == "AR"||
+        $country == "BO"||
+        $country == "CL"||
+        $country == "CO"||
+        $country == "CR"||
+        $country == "CU"||
+        $country == "DO"||
+        $country == "EC"||
+        $country == "ES"||
+        $country == "GT"||
+        $country == "HN"||
+        $country == "MX"||
+        $country == "NI"||
+        $country == "PA"||
+        $country == "PE"||
+        $country == "PR"||
+        $country == "PY"||
+        $country == "SV"||
+        $country == "UY"||
+        $country == "VE"
+      )
+      {
+          $lang = "es";
+      }
+      if($country == "EE"){
+        $lang = "et";
+      }
+      if($country == "IR"){
+        $lang = "fa";
+      }
+      if($country == "FI"){
+        $lang = "fi";
+      }
+      if($country == "FO"){
+        $lang = "fo";
+      }
+      if(
+        $country == "BE"||
+        $country == "FR"||
+        $country == "SN"
+      )
+      {
+        $lang = "fr";
+      }
+      if($country == "IL"){
+        $lang = "he";
+      }
+      if($country == "IN"){
+        $lang = "hi";
+      }
+      if($country == "HR"){
+        $lang = "hr";
+      }
+      if($country == "HT"){
+        $lang = "ht";
+      }
+      if($country == "HU"){
+        $lang = "hu";
+      }
+      if($country == "AM"){
+        $lang = "hy";
+      }
+      if($country == "ID"){
+        $lang = "id";
+      }
+      if($country == "IS"){
+        $lang = "is";
+      }
+      if($country == "IT"){
+        $lang = "it";
+      }
+      if($country == "JP"){
+        $lang = "ja";
+      }
+      if($country == "GE"){
+        $lang = "ka";
+      }
+      if($country == "KZ"){
+        $lang = "kk";
+      }
+      if($country == "GL"){
+        $lang = "kl";
+      }
+      if($country == "KH"){
+        $lang = "km";
+      }
+      if($country == "KR"){
+        $lang = "ko";
+      }
+      if($country == "KG"){
+        $lang = "ky";
+      }
+      if($country == "UG"){
+        $lang = "lg";
+      }
+      if($country == "LA"){
+        $lang = "lo";
+      }
+      if($country == "LT"){
+        $lang = "lt";
+      }
+      if($country == "LV"){
+        $lang = "lv";
+      }
+      if($country == "MG"){
+        $lang = "mg";
+      }
+      if($country == "MK"){
+        $lang = "mk";
+      }
+      if($country == "MN"){
+        $lang = "mn";
+      }
+      if($country == "MY"){
+        $lang = "ms";
+      }
+      if($country == "MT"){
+        $lang = "mt";
+      }
+      if($country == "MM"){
+        $lang = "my";
+      }
+      if($country == "NP"){
+        $lang = "ne";
+      }
+      if($country == "AW" || $country == "NL"){
+        $lang = "nl";
+      }
+      if($country == "NO"){
+        $lang = "no";
+      }
+      if($country == "PL"){
+        $lang = "pl";
+      }
+      if($country == "AF"){
+        $lang = "ps";
+      }
+      if($country == "AF"){
+        $lang = "ps";
+      }
+      if($country == "AO" || $country == "BR"  || $country == "PT"){
+        $lang = "pt";
+      }
+      if($country == "RO"){
+        $lang = "ro";
+      }
+      if($country == "RU" || $country == "UA"){
+        $lang = "ru";
+      }
+      if($country == "RW"){
+        $lang = "rw";
+      }
+      if($country == "AX"){
+        $lang = "se";
+      }
+      if($country == "SK"){
+        $lang = "sk";
+      }
+      if($country == "SI"){
+        $lang = "sl";
+      }
+      if($country == "SO"){
+        $lang = "so";
+      }
+      if($country == "AL"){
+        $lang = "sq";
+      }
+      if($country == "ME" || $country == "RS"){
+        $lang = "sr";
+      }
+      if($country == "SE"){
+        $lang = "sv";
+      }
+      if($country == "TZ"){
+        $lang = "sw";
+      }
+      if($country == "LK"){
+        $lang = "ta";
+      }
+      if($country == "TJ"){
+        $lang = "tg";
+      }
+      if($country == "TH"){
+        $lang = "th";
+      }
+      if($country == "TM"){
+        $lang = "tk";
+      }
+      if($country == "CY" || $country == "TR"){
+        $lang = "tr";
+      }
+      if($country == "PK"){
+        $lang = "ur";
+      }
+      if($country == "UZ"){
+        $lang = "uz";
+      }
+      if($country == "VN"){
+        $lang = "vi";
+      }
+      if($country == "CN" || $country == "HK" || $country == "TW"){
+        $lang = "zh";
+      }
